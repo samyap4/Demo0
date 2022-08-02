8ecc2ce0425b487ee95428b11bb4f04d19f7ee18
Sam Yapkowitz <samyapkowitz@gmail.com>
2022-08-02 17:55:57 -0500

    adding orgs login button

--- Web/src/App.js
+++ Web/src/App.js
@@ -246,6 +246,14 @@ export default function App() {
               >
                 Issue MFA
               </button>
+              <br></br>
+              <br></br>
+              <button 
+                onClick={()=>loginWithRedirect({organization: 'org_3vMJmTZoFIpZ1tp5'})} 
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Login to Team B
+              </button>
           </>
           <br/>
           <br/>
