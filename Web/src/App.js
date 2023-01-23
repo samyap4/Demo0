030a80dbf4f8b65c793ae5b6879f110641452581
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-01-22 22:33:34 -0600

    Update App.js

--- Web/src/App.js
+++ Web/src/App.js
@@ -37,6 +37,10 @@ export default function App() {
     window.open('https://auth.sam-yap.com/samlp/2OoBeHp2MThFiczdXVdrTJh5YHIE9p85', '_blank');
   }
 
+  const goToOktaReact = () => {
+    window.open('https://auth.sam-yap.com/samlp/UzuMwOYAuauzsOBMOPY2esnrN9978oxE?RelayState=https://okta-react.sam-yap.com/sso/callback', '_blank');
+  }
+
   const classNames = (...classes) => {
     return classes.filter(Boolean).join(' ')
   }
@@ -255,8 +259,15 @@ export default function App() {
           <hr/>
           <br/>
           <>
+          <button 
+                onClick={()=>goToOktaReact()} 
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                Partner App
+                </button>
             <button 
                 onClick={()=>goToDatadog()} 
+                style={{display: 'inline-block', marginLeft: '10px'}}
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                 View Logs (SSO)
