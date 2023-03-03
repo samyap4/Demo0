b6afc39e0370e3d734a52cb3552037fa36ea656d
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-03-03 15:32:58 -0600

    Update Home.js

--- Web/src/components/Home.js
+++ Web/src/components/Home.js
@@ -189,6 +189,13 @@ export default function Home() {
           Login
         </button>
         <button 
+          onClick={()=>loginWithRedirect({connection: 'custom-db'})} 
+          style={{display: 'inline-block', marginLeft: '10px'}}
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Login w Custom DB
+        </button>
+        <button 
           onClick={()=>loginWithRedirect({organization: 'org_uTGQp17SrA1PX0tY'})} 
           style={{display: 'inline-block', marginLeft: '10px'}}
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
