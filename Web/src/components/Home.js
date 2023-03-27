fe935fe0f4905080f77078e3f0de2baafe13c40a
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-03-27 11:49:06 -0500

    Update Home.js

--- Web/src/components/Home.js
+++ Web/src/components/Home.js
@@ -232,11 +232,11 @@ export default function Home() {
           Login
         </button>
         <button 
-          onClick={()=>loginWithRedirect({connection: 'custom-db'})} 
+          onClick={()=>loginWithRedirect({connection: 'OktaSAML'})} 
           style={{display: 'inline-block', marginLeft: '10px'}}
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Login w Custom DB
+          Login w SSO
         </button>
         <button 
           onClick={()=>loginWithRedirect({organization: 'org_uTGQp17SrA1PX0tY'})} 
