1655d3e4fb48442c712844b67963b6e098e22dca
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-11-27 08:52:29 -0600

    Update Home.js

--- Web/src/components/Home.js
+++ Web/src/components/Home.js
@@ -266,6 +266,15 @@ export default function Home() {
                     </button>
             );
           })}
+          {companyId === 'org_RUz5Akf1AnP7YnqQ' &&
+          <button 
+            onClick={() => loginWithRedirect({connection: 'Lululemon'})} 
+            style={{display: 'inline-block', marginLeft: '10px'}}
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login w SSO
+        </button>
+          }
         </>
       }
       {!user && errorDescription && 
