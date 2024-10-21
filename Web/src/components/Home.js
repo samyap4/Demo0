541b0c55638bad927e762ea0b469dcc9ecdaa067
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-10-21 13:47:12 -0500

    Update Home.js

--- Web/src/components/Home.js
+++ Web/src/components/Home.js
@@ -367,6 +367,18 @@ export default function Home() {
               >
                 Logout
               </button>
+              <button
+                onClick={() =>
+                  loginWithRedirect({
+                    acr_values:
+                      "http://schemas.openid.net/pape/policies/2007/06/multi-factor",
+                  })
+                }
+                style={{ display: "inline-block", marginLeft: "10px" }}
+                class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Issue MFA
+              </button>
             </div>
             <br />
             <hr />
