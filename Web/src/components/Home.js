c1414512808d9e8c9f7ede5ec18e9c19e8f82914
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-11-18 21:04:45 -0600

    Update Home.js

--- Web/src/components/Home.js
+++ Web/src/components/Home.js
@@ -127,7 +127,7 @@ export default function Home() {
         }
       });
     }
-  }, [errorDescription, isLoading, isAuthenticated]);
+  }, [errorDescription, isLoading, isAuthenticated, user]);
 
   const logoutGlobally = () => {
     setLoginData(null);
