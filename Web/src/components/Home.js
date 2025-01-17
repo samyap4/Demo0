91e0a4e4827488c67c0692afd1cf0fd2de19a380
Sam Yapkowitz <samyapkowitz@gmail.com>
2025-01-17 10:57:27 -0600

    Update Home.js

--- Web/src/components/Home.js
+++ Web/src/components/Home.js
@@ -37,12 +37,12 @@ export default function Home() {
     );
     console.log(auth0Values);
     let rawToken = JSON.parse(auth0Values)?.body?.access_token;
-    if (rawToken === null || rawToken === undefined) {
-      console.log("getting new tokens");
-      rawToken = await getAccessTokenSilently({
-        audience: "https://api.superwidget",
-      });
-    }
+    // if (rawToken === null || rawToken === undefined) {
+    //   console.log("getting new tokens");
+    //   rawToken = await getAccessTokenSilently({
+    //     audience: "https://api.superwidget",
+    //   });
+    // }
     setAccessToken(jwt_decode(rawToken));
   }, []);
 
